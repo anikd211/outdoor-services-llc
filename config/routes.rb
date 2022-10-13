8b381b939f2b23735d6e1653dbe371c17b01302c
Ubuntu <ubuntu@ip-172-31-57-36.ec2.internal>
2022-10-13 19:31:10 +0000

    Working site naviagtion

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root to: "static_pages#home"
-  get 'static_pages/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root  "static_pages#home"
+  get '/contact', to: 'static_pages#contact'
 end
